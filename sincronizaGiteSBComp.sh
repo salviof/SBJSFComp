6b92e636bc4141f6ab960c7447d49444ed5f21c3
Salvio Furbino <salviof@gmail.com>
2016-06-05 16:23:44 -0300

    Updated master branch of deployment repo to point to latest head of submodules

--- sincronizaGiteSBComp.sh
+++ sincronizaGiteSBComp.sh
@@ -1,9 +1,20 @@
-find . -name target -type d -exec rm -rf {} \;
+git checkout master  && git pull --ff origin master
 
-git pull
-git add --all
-git commit -m "Atualizacao automática"
-git push
+git submodule sync
+git submodule init
+git submodule update
+git submodule foreach "(git checkout master && git pull --ff origin master && git push origin master) || true"
+
+for i in $(git submodule foreach --quiet 'echo ./')
+do
+  echo "Adding $i to root repo"
+  git add "$i"
+done
+
+git commit -m "Updated master branch of deployment repo to point to latest head of submodules"
+git push origin master
+
+
 
 cd webApp/src/main/webapp/resources/SBComp/
 ./sincroniza.sh
