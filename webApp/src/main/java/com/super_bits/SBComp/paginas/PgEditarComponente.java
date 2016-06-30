62a8486c9b6c865ba08078f17e86bc8df1057cba
Salvio Furbino <salviof@gmal.com>
2016-06-30 09:11:18 -0300

    Updated master branch of deployment repo to point to latest head of submodules

--- webApp/src/main/java/com/super_bits/SBComp/paginas/PgEditarComponente.java
+++ webApp/src/main/java/com/super_bits/SBComp/paginas/PgEditarComponente.java
@@ -5,14 +5,10 @@
  */
 package com.super_bits.SBComp.paginas;
 
-import com.super_bits.Controller.Interfaces.permissoes.ItfAcaoFormulario;
 import com.super_bits.InomeClienteI.editorCompoente.ComponenteVisual;
 import com.super_bits.InomeClienteI.editorCompoente.regras_de_negocio_e_controller.FabAcaoEditorDeComponentes;
 import com.super_bits.InomeClienteI.editorCompoente.regras_de_negocio_e_controller.InfoAcaoEditorComponente;
-import com.super_bits.modulos.SBAcessosModel.controller.FabAcaoSeguranca;
-import com.super_bits.modulos.SBAcessosModel.model.UsuarioSB;
 import com.super_bits.modulos.SBAcessosModel.model.acoes.AcaoDoSistema;
-import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
 import com.super_bits.modulosSB.SBCore.InfoCampos.campo.CaminhoCampoReflexao;
 import com.super_bits.InomeClienteI.editorCompoente.model.BeanExemplo;
 import com.super_bits.modulosSB.webPaginas.JSFBeans.SB.siteMap.MB_paginaCadastroEntidades;
