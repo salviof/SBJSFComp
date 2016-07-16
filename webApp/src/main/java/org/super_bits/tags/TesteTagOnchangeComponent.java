60a3297986931649d4e7b6cdab54294b4ccf595e
Salvio Furbino <salviof@gmal.com>
2016-07-16 02:12:04 -0300

    Atualizacao automática

--- webApp/src/main/java/org/super_bits/tags/TesteTagOnchangeComponent.java
+++ webApp/src/main/java/org/super_bits/tags/TesteTagOnchangeComponent.java
@@ -6,13 +6,57 @@
 package org.super_bits.tags;
 
 import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponentBase;
+import javax.faces.component.UINamingContainer;
 import javax.faces.component.UIPanel;
+import javax.faces.context.FacesContext;
+import org.primefaces.component.api.Widget;
 
 /**
  *
  * @author salvioF
  */
 @FacesComponent("testeTagOnChange")
-public class TesteTagOnchangeComponent extends UIPanel {
+public class TesteTagOnchangeComponent extends UIComponentBase implements Widget {
+
+    public static final String CLASSE_DO_COMPONENTE = "org.super_bits.tags.TesteTagOnchangeComponent";
+    public static final String FAMILIA_DO_COMPONENTE = "org.super_bits.tags.componentes";
+
+    @Override
+    public String getFamily() {
+        return FAMILIA_DO_COMPONENTE;
+    }
+
+    /**
+     * Através dessa variavel é possivel acessar o objeto via javascript
+     *
+     * @return
+     */
+    @Override
+    public String resolveWidgetVar() {
+        FacesContext context = getFacesContext();
+        String userWidgetVar = (String) getAttributes().get("widgetVar");
+
+        if (userWidgetVar != null) {
+            return userWidgetVar;
+        } else {
+            return "widget_"
+                    + getClientId(context).replaceAll(
+                    "-|" + UINamingContainer.getSeparatorChar(context),
+                    "_");
+        }
+    }
+
+    protected static enum PropertyKeys {
+        metodoOnchange, widgetVar, propriedadeTextoTeste;
+    }
+
+    public String getWidgetVar() {
+        return (String) getStateHelper().eval(PropertyKeys.widgetVar, null);
+    }
+
+    public void setWidgetVar(String _widgetVar) {
+        getStateHelper().put(PropertyKeys.widgetVar, _widgetVar);
+    }
 
 }
