8435ab2d435e19bcd4c51a0f43640e7def433b4c
Salvio Furbino <salviof@gmal.com>
2016-07-26 01:44:50 -0300

    Atualizacao automática

--- webApp/src/main/java/org/super_bits/tags/InputSBRender.java
+++ webApp/src/main/java/org/super_bits/tags/InputSBRender.java
@@ -32,47 +32,19 @@ public class InputSBRender extends CoreRenderer {
 
         ResponseWriter writer = context.getResponseWriter();
 
-        Object value = inputSB.getValue() != null ? inputSB.getValue() : 0;
-
+        //Object value = inputSB.getValue() != null ? inputSB.getValue() : 0;
+        // writer.startElement(RENDERER_TYPE,);
         writer.startElement("input", inputSB);
         writer.writeAttribute("id", inputSB.getClientId(), null);
         writer.writeAttribute("name", inputSB.getClientId(), null);
         writer.writeAttribute("disabled", true, null);
-        writer.writeAttribute("value", value.toString(), null);
-        writer.writeAttribute("data-min", inputSB.getMin(), null);
-        writer.writeAttribute("data-step", inputSB.getStep(), null);
-        writer.writeAttribute("data-max", inputSB.getMax(), null);
-        writer.writeAttribute("data-displayInput", Boolean.toString(inputSB.isShowLabel()), null);
-        writer.writeAttribute("data-readOnly", Boolean.toString(inputSB.isDisabled()), null);
-        writer.writeAttribute("data-cursor", Boolean.toString(inputSB.isCursor()), null);
-
-        if (inputSB.getThickness() != null) {
-            writer.writeAttribute("data-thickness", inputSB.getThickness(), null);
-        }
-
-        if (inputSB.getForegroundColor() != null) {
-            String fg;
-            if (inputSB.getForegroundColor() instanceof Color) {
-                fg = "FFFFFF";
-            } else {
-                fg = inputSB.getForegroundColor().toString();
-            }
-            writer.writeAttribute("data-fgColor", fg, null);
-        }
-
-        if (inputSB.getBackgroundColor() != null) {
-            String bg;
-            if (inputSB.getBackgroundColor() instanceof Color) {
-                bg = "ffffff";
-            } else {
-                bg = inputSB.getBackgroundColor().toString();
-            }
-            writer.writeAttribute("data-bgColor", bg, null);
-        }
-
-        if (inputSB.getWidth() != null) {
-            writer.writeAttribute("data-width", inputSB.getWidth().toString(), null);
-        }
+        //  writer.writeAttribute("value", value.toString(), null);
+        //   writer.writeAttribute("data-min", inputSB.getMin(), null);
+        //   writer.writeAttribute("data-step", inputSB.getStep(), null);
+        //   writer.writeAttribute("data-max", inputSB.getMax(), null);
+        //writer.writeAttribute("data-displayInput", Boolean.toString(inputSB.isShowLabel()), null);
+        // writer.writeAttribute("data-readOnly", Boolean.toString(inputSB.isDisabled()), null);
+        //  writer.writeAttribute("data-cursor", Boolean.toString(inputSB.isCursor()), null);
 
         writer.writeAttribute("class", "knob", null);
 
@@ -82,14 +54,14 @@ public class InputSBRender extends CoreRenderer {
         writer.writeAttribute("id", inputSB.getClientId() + "_hidden", null);
         writer.writeAttribute("name", inputSB.getClientId() + "_hidden", null);
         writer.writeAttribute("type", "hidden", null);
-        writer.writeAttribute("value", value.toString(), null);
+        // writer.writeAttribute("value", value.toString(), null);
         writer.endElement("input");
     }
 
     @Override
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
         this.encodeMarkup(context, (InputSB) component);
-        this.encodeScript(context, (InputSB) component);
+        //   this.encodeScript(context, (InputSB) component);
     }
 
 }
